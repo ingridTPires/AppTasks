1e85940e1b875821206bfd3fa08df458e4e41957
IngridTPires <ingrid.teixeirap@gmail.com>
2024-07-14 23:36:47 -0300

    Registro de log de consumers

--- AppTasks/NotificationService/Consumers/UserCreatedConsumer.cs
+++ AppTasks/NotificationService/Consumers/UserCreatedConsumer.cs
@@ -7,14 +7,18 @@ namespace NotificationService.Consumers
     public class UserCreatedConsumer : IConsumer<UserCreated>
     {
         private readonly IMailService _mailService;
+        private readonly ILogger<UserCreatedConsumer> _logger;
         private const string SUBJECT = "Novo Usuário";
         private const string MESSAGE = "Olá {0}, <br />Seu cadastro foi realizado com sucesso!";
-        public UserCreatedConsumer(IMailService mailService)
+        public UserCreatedConsumer(IMailService mailService, ILogger<UserCreatedConsumer> logger)
         {
             _mailService = mailService;
+            _logger = logger;
         }
         public async Task Consume(ConsumeContext<UserCreated> context)
         {
+            _logger.LogInformation($"event: usuário recebido {context.Message.Id}");
+
             var message = string.Format(MESSAGE, context.Message.Name);
             await _mailService.Send(context.Message.Email, message, SUBJECT);
         }
--- AppTasks/NotificationService/Consumers/UserTaskCreatedConsumer.cs
+++ AppTasks/NotificationService/Consumers/UserTaskCreatedConsumer.cs
@@ -7,14 +7,18 @@ namespace NotificationService.Consumers
     public class UserTaskCreatedConsumer : IConsumer<UserTaskCreated>
     {
         private readonly IMailService _mailService;
+        ILogger<UserTaskCreatedConsumer> _logger;
         private const string SUBJECT = "Nova Tarefa";
         private const string MESSAGE = "Você possui uma nova tarefa!<br />Título: {0}<br />Descrição:{1}";
-        public UserTaskCreatedConsumer(IMailService mailService)
+        public UserTaskCreatedConsumer(IMailService mailService, ILogger<UserTaskCreatedConsumer> logger)
         {
             _mailService = mailService;
+            _logger = logger;
         }
         public async Task Consume(ConsumeContext<UserTaskCreated> context)
         {
+            _logger.LogInformation($"event: tarefa recebida {context.Message.Id} para o usuário {context.Message.UserMail}");
+
             var message = string.Format(MESSAGE, context.Message.Title, context.Message.Description);
             await _mailService.Send(context.Message.UserMail, message, SUBJECT);
         }
